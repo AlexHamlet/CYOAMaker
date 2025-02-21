fcfa129e8ee65e1480cb8a2e090dfaa1dd453b14
Alex Hamlet <alexhamlet@comcast.net>
2025-02-20 21:59:05 -0700

    Started on User Friendly Editor

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import StoryEditor from './components/StoryEditor.vue';
 import DevStoryEditor from './components/DevStoryEditor.vue';
 </script>
 
 <template>
+  <!-- <StoryEditor></StoryEditor> -->
   <DevStoryEditor></DevStoryEditor>
 </template>
 
---
+++ src/components/StoryEditor.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-content">
+    <EditPage></EditPage>
+  </div>
+  <div class="sidebar">
+    <StoryTreeView></StoryTreeView>
+  </div>
+</template>
+
+<script setup lang="ts">
+import EditPage from './StoryEditor/EditPage.vue';
+import StoryTreeView from './StoryView/StoryTreeView.vue';
+</script>
+
+<style scoped>
+.main-content {
+  margin-left: 260px;
+  /* Pushes content right, should match sidebar width + padding */
+  padding: 20px;
+}
+
+
+.sidebar {
+  margin: 0;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 250px;
+  height: 100vh;
+  background: #f0f0f0;
+  overflow-y: auto;
+  border-right: 2px solid #ccc;
+}
+</style>
